cd7ef5c96b548b1b335263a659249d2cf0e756b6
knasti <knastman1@gmail.com>
2017-03-23 10:45:38 +0100

    Created a new folder for the python scripts

---
+++ network/example.py
@@ -0,0 +1,32 @@
+from database import Database
+from database import CursorFromConnectionFromPool
+
+
+class User:
+    def __init__(self, screen_name, oauth_token, oauth_token_secret, id):
+        self.screen_name = screen_name
+        self.oauth_token = oauth_token
+        self.oauth_token_secret = oauth_token_secret
+        self.id = id
+
+    def __repr__(self):
+        return "<User {}>".format(self.screen_name)
+
+    def save_to_db(self):
+        with CursorFromConnectionFromPool() as cursor:
+            cursor.execute('INSERT INTO users (screen_name, oauth_token, oauth_token_secret) VALUES (%s, %s, %s)',
+                               (self.screen_name, self.oauth_token, self.oauth_token_secret))
+
+    @classmethod
+    def load_from_db_by_screen_name(cls, screen_name):
+        with CursorFromConnectionFromPool() as cursor:
+            cursor.execute('SELECT * FROM users WHERE screen_name=%s', (screen_name,))
+            user_data = cursor.fetchone()
+            if user_data: # None is equilavent to false in boolaen expressions
+                return cls(screen_name=user_data[1], oauth_token=user_data[2],
+                           oauth_token_secret=user_data[3], id=user_data[0])
+
+
+Database.initialise(user='postgres', password='postgres', host='localhost', database='learning')
+
+user = User('blabla','blablabla','blablablabla')
